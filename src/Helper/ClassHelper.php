443e3cd545827e8eb2256a1c85e812f937de98f6
Artem Vasilev <kern.usr@gmail.com>
2022-12-21 14:09:07 +0300

    ClassHelper

---
+++ src/Helper/ClassHelper.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Webmasterskaya\Soap\Base\Helper;
+
+class ClassHelper
+{
+	public static function shouldImplement($class, $interface)
+	{
+		$interfaces = class_implements($class);
+
+		return isset($interfaces[$interface]);
+	}
+
+	public static function shouldNotImplement($class, $interface)
+	{
+		return !static::shouldImplement($class, $interface);
+	}
+
+	public static function shouldBeAnInstanceOf($class, $instance)
+	{
+		if (is_object($class))
+		{
+			return $class instanceof $instance;
+		}
+
+		if ($class == $instance)
+		{
+			return true;
+		}
+
+		$parents         = class_parents($class);
+		$parents[$class] = $class;
+
+		return isset($parents[$instance]);
+	}
+
+	public static function shouldNotBeAnInstanceOf($class, $instance)
+	{
+		return !static::shouldBeAnInstanceOf($class, $instance);
+	}
+}
