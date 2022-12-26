4c8fdd9524ed9c744070b1ddf87797f7176ca1de
Artem Vasilev <kern.usr@gmail.com>
2022-12-26 21:38:07 +0300

    SOAP Exception

---
+++ src/Exception/SoapException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Webmasterskaya\Soap\Base\Exception;
+
+class SoapException extends RuntimeException
+{
+    /**
+     * @param \Throwable $throwable
+     *
+     * @return SoapException
+     */
+    public static function fromThrowable(\Throwable $throwable): self
+    {
+        return new self($throwable->getMessage(), (int)$throwable->getCode(), $throwable);
+    }
+}
