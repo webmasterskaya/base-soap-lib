fb2be3b324aa356c8845852885f65f2c5db48abe
Artem Vasilev <kern.usr@gmail.com>
2022-12-27 13:06:42 +0300

    ClientFactory

--- src/ClientFactoryAbstract.php
+++ src/ClientFactoryAbstract.php
@@ -6,6 +6,8 @@ use Soap\Engine\Transport;
 use Soap\ExtSoapEngine\Configuration\ClassMap\ClassMap;
 use Soap\ExtSoapEngine\Configuration\ClassMap\ClassMapCollection;
 use Soap\ExtSoapEngine\Configuration\ClassMap\ClassMapInterface;
+use Soap\ExtSoapEngine\Configuration\TypeConverter\TypeConverterCollection;
+use Soap\ExtSoapEngine\Configuration\TypeConverter\TypeConverterInterface;
 use Soap\ExtSoapEngine\ExtSoapOptions;
 use Soap\ExtSoapEngine\Wsdl\PassThroughWsdlProvider;
 use Soap\ExtSoapEngine\Wsdl\WsdlProvider;
@@ -47,7 +49,7 @@ abstract class ClientFactoryAbstract implements ClientFactoryInterface
                 $classMap()
             );
         } else {
-            $clientClassMap = call_user_func([static::class, 'getDefaultClientTypeMap'])();
+            $clientClassMap = call_user_func([static::class, 'getDefaultClientClassMap'])();
         }
 
         $clientOptions->withClassMap($clientClassMap);
@@ -93,6 +95,38 @@ abstract class ClientFactoryAbstract implements ClientFactoryInterface
         return new ClassMapCollection(...array_values($merged));
     }
 
+    public static function mergeTypeMapCollections(
+        TypeConverterCollection ...$typeConverterCollections
+    ): TypeConverterCollection {
+        $numArgs = func_num_args();
+        if ($numArgs < 1) {
+            throw new InvalidArgumentException(
+                sprintf('"%s::mergeTypeMapCollections()" expects at least 1 parameters, %s given',
+                    static::class,
+                    $numArgs
+                ));
+        }
+
+        /** @var array<TypeConverterInterface> $merged */
+        $merged = [];
+
+        foreach ($typeConverterCollections as $typeConverterCollection) {
+            /** @var array<string, TypeConverterInterface> $typeMaps */
+            $typeMaps = array_map(
+                static function (TypeConverterInterface $converter) {
+                    return [
+                        strtolower($converter->getTypeName()),
+                        $converter
+                    ];
+                },
+                iterator_to_array($typeConverterCollection)
+            );
+            $merged = array_merge($merged, $typeMaps);
+        }
+
+        return new TypeConverterCollection(array_values($merged));
+    }
+
     public static function getClientClassName(): string
     {
         $classname = static::$clientClass;
@@ -113,5 +147,25 @@ abstract class ClientFactoryAbstract implements ClientFactoryInterface
         }
 
         return $classname;
+    }
+
+    public static function getEmptyClassMap(): ClientClassMapCollectionInterface
+    {
+        return new class implements ClientClassMapCollectionInterface {
+            public function __invoke(): ClassMapCollection
+            {
+                return new ClassMapCollection();
+            }
+        };
+    }
+
+    public static function getEmptyTypeMap(): ClientTypeConverterCollectionInterface
+    {
+        return new class implements ClientTypeConverterCollectionInterface {
+            public function __invoke(): TypeConverterCollection
+            {
+                return new TypeConverterCollection();
+            }
+        };
     }
 }
