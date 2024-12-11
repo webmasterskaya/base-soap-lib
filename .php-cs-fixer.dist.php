6517a4e8e7f845c4e2c6a9c470359066e7b5a3a5
Artem Vasilev <kern.usr@gmail.com>
2024-12-11 14:35:47 +0300

    Dev dependencies and script in composer.json file

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -13,13 +13,6 @@ return (new PhpCsFixer\Config())
         '@PSR12' => true,
         'align_multiline_comment' => true,
         'array_indentation' => true,
-        'declare_strict_types' => false,
-        'final_class' => false,
-        'global_namespace_import' => [
-            'import_classes' => true,
-            'import_constants' => true,
-            'import_functions' => true,
-        ],
         'list_syntax' => [
             'syntax' => 'short',
         ],
